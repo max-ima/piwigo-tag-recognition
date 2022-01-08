5fbcbdfa0fd5a4b685e1df0718d3d85e7ad1dc23
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-08 14:40:16 +0100

    New language sl_SI

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,19 @@
+<?php
+$lang['Use this API'] = "Uporabite ta API";
+$lang['Generate tags'] = "Ustvari oznake";
+$lang['Generate tags for this image'] = "Ustvari oznake za to sliko";
+$lang['Maximum number'] = "Največje število";
+$lang['Language code'] = "Jezikovna koda";
+$lang['%d request left this month'] = "%d zahteva je ostala ta mesec";
+$lang['There is an error with the API'] = "Prišlo je do napake z API-jem";
+$lang['No more requests left this month'] = "Ta mesec ni več zahtev";
+$lang['Proposed tags'] = "Predlagane oznake";
+$lang['Click on tags to select them'] = "Kliknite na oznake, da jih izberete";
+$lang['Apply tags'] = "Uporabi oznake";
+$lang['Fetching the API...'] = "Pridobivanje API-ja ...";
+$lang['Tags successfully added'] = "Oznake so bile uspešno dodane";
+
+//missing
+$lang['API Key'] = "API ključ";
+$lang['API Secret'] = "Skrivnostni API";
+?>
