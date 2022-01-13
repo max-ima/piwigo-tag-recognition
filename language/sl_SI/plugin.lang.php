d600796403c0579dce0a48b132dfb99ef590813e
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-13 10:18:02 +0100

    missing
    Updated wmissing

--- language/sl_SI/plugin.lang.php
+++ language/sl_SI/plugin.lang.php
@@ -1,7 +1,9 @@
 <?php
+$lang['Tag Recognition'] = "Prepoznavanje oznak";
 $lang['Use this API'] = "Uporabite ta API";
 $lang['Generate tags'] = "Ustvari oznake";
 $lang['Generate tags for this image'] = "Ustvari oznake za to sliko";
+$lang['Loading...'] = "Nalaganje...";
 $lang['Maximum number'] = "Največje število";
 $lang['Language code'] = "Jezikovna koda";
 $lang['%d request left this month'] = "%d zahteva je ostala ta mesec";
@@ -10,10 +12,18 @@ $lang['No more requests left this month'] = "Ta mesec ni več zahtev";
 $lang['Proposed tags'] = "Predlagane oznake";
 $lang['Click on tags to select them'] = "Kliknite na oznake, da jih izberete";
 $lang['Apply tags'] = "Uporabi oznake";
+$lang['Cancel'] = "Prekliči";
 $lang['Fetching the API...'] = "Pridobivanje API-ja ...";
 $lang['Tags successfully added'] = "Oznake so bile uspešno dodane";
+$lang['Ok'] = "V redu";
+$lang['Applies auto-generated tags to images'] = "Uporabi samodejno ustvarjene oznake za slike";
+$lang['Number of tags'] = "Število oznak";
+$lang['You haven\'t enough request left this month for this action'] = "Ta mesec vam ni ostalo dovolj zahtev za to dejanje";
+$lang['Tag successfully generated and added'] = "Oznaka je bila uspešno ustvarjena in dodana";
 
-//missing
+//admin
+$lang['This API is used'] = "Ta API se uporablja";
 $lang['API Key'] = "API ključ";
 $lang['API Secret'] = "Skrivnostni API";
+$lang['Save Settings'] = "Shrani nastavitve";
 ?>
