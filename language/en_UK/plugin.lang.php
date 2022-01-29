3227238c9b17e784010291dd0575c6292206082b
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-29 18:27:50 +0100

    missing
    missing

--- language/en_UK/plugin.lang.php
+++ language/en_UK/plugin.lang.php
@@ -2,6 +2,7 @@
 $lang['Use this API'] = "Use this API";
 $lang['Generate tags'] = "Generate tags";
 $lang['Generate tags for this image'] = "Generate tags for this image";
+$lang['Loading...'] = "Loading...";
 $lang['Maximum number'] = "Maximum number";
 $lang['Language code'] = "Language code";
 $lang['%d request left this month'] = "%d request left this month";
@@ -12,4 +13,10 @@ $lang['Click on tags to select them'] = "Click on tags to select them";
 $lang['Apply tags'] = "Apply tags";
 $lang['Fetching the API...'] = "Fetching the API...";
 $lang['Tags successfully added'] = "Tags successfully added";
+
+//admin
+$lang['This API is used'] = "This API is used";
+$lang['API Key'] = "API Key";
+$lang['API Secret'] = "API Secret";
+$lang['Save Settings'] = "Save Settings";
 ?>
